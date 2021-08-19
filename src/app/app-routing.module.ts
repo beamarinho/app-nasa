d5cdd7c80005033106fa6b18507b78c289e45b25
beamarinho <beatrizmarinh@gmail.com>
2021-08-19 10:47:57 -0300

    app nasa, primeiro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'show-image',
+    loadChildren: () => import('./pages/show-image/show-image.module').then( m => m.ShowImagePageModule)
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,19 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HTTP } from '@ionic-native/http/ngx';
+import { ModalInfoComponent } from './component/modal-info/modal-info.component';
+import { File } from '@ionic-native/file/ngx';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { FileTransfer} from '@ionic-native/file-transfer/ngx';
+import { Downloader } from '@ionic-native/downloader/ngx';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ModalInfoComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule],
+  providers: [HTTP, File,FileTransfer, Downloader,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
